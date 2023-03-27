cb3ad3913a6f6a842165ef2d0d906a18e752d5a9
Kevin <kemcnulty@csumb.edu>
2023-03-27 16:23:35 -0700

    This class both sends message for new enrollment and receives message of final grades to/from the gradebook backend.

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -32,9 +32,10 @@ public class GradebookServiceMQ extends GradebookService {
 	// send message to grade book service about new student enrollment in course
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
-		 
-		// TODO 
-		// create EnrollmentDTO and send to gradebookQueue
+	
+		EnrollmentDTO enrollmentDTO = new EnrollmentDTO(student_email, student_name, course_id);
+		rabbitTemplate.convertAndSend(gradebookQueue.getName(), enrollmentDTO);
+
 		
 		System.out.println("Message send to gradbook service for student "+ student_email +" " + course_id);  
 		
@@ -44,8 +45,14 @@ public class GradebookServiceMQ extends GradebookService {
 	public void receive(CourseDTOG courseDTOG) {
 		System.out.println("Receive enrollment :" + courseDTOG);
 
-		//TODO 
+		
 		// for each student grade in courseDTOG,  find the student enrollment entity, update the grade and save back to enrollmentRepository.
+		for (CourseDTOG.GradeDTO g : courseDTOG.grades) {
+			Enrollment e = enrollmentRepository.findByEmailAndCourseId(g.student_email, courseDTOG.course_id);
+			e.setCourseGrade(g.grade);
+			enrollmentRepository.save(e); 
+			System.out.println("final grade update " + g.student_email + " " + courseDTOG.course_id + " " + g.grade);
+		}
 	}
 
 }
